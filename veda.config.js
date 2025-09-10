a1cf06c1e9d4283d31373ea6d31ee40a5fd69d2e
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-09-10 10:31:22 -0500

    change navheader

--- veda.config.js
+++ veda.config.js
@@ -115,7 +115,7 @@ module.exports = {
     'externalLinksInNewTab': true,
   },
   navItems: {
-    mainNavItems: defaultMenuLinks,
+    headerNavItems: defaultMenuLinks,
     subNavItems: subNavItems
   },
 };
